186b5dccd0cdbdf8cbac4f22910e49378e659d49
立淳 <richy631@gmail.com>
2016-06-16 20:33:43 +0800

    if the priority is correct, i think we will done!

--- test.m
+++ test.m
@@ -1,8 +1,9 @@
 clear;
 clear all;
-imageName = 'bungee';
+%imageName = 'bungee';
 %imageName = 'air';
-originImage = imread([imageName, '.png']);
+imageName = 'aircondition';
+originImage = imread([imageName, '.jpg']);
 
 %如果要重畫mask，把以下的註解取消
 %if you want to redraw mask, cancel the comment below
@@ -61,13 +62,6 @@ data = gradtnorm + log10(1+gradtnorm);
 %figure;imshow(fy);
 %figure;imshow(data);title('data');
 
-
-%change RGB to LAB
-colorTransform = makecform('srgb2lab');
-maskedImage = applycform(maskedImage , colorTransform);
-
-
-
 %計算confidence (patch裡面原圖與mask的比例)
 % compute "confidence 
 confidence = double(1-mask);
@@ -108,13 +102,19 @@ end
 
 %figure;imshow(priority);title('priority');
 
+%change RGB to LAB
+colorTransform = makecform('srgb2lab');
+maskedImage = applycform(maskedImage , colorTransform);
+
 %開始inpainting了
 %START TO INPAINTING
 
-while ~isequal(mask, zeros(m,n) ) %as research 等到 omega = 0為止
-                                  %omega 就是這裡的mask
-                                  %as sesearch, until omdga = 0
-                                  %omega is mask here
+while (indexM ~= 0) %as research 等到 omega = 0為止
+                    %相當於indexM沒有index了
+                    %omega 就是這裡的mask
+                    %as sesearch, until omdga = 0
+                    %indexM == 0
+                    %omega is mask here
 	
     %STEP1 find all index in mask
     
@@ -137,36 +137,35 @@ while ~isequal(mask, zeros(m,n) ) %as research 等到 omega = 0為止
     %STEP2 compute the priority
     
     priority = data.* confidence;
-    [priX, priY] = size(priority);
-    
+
     %STEP3 find the patch with maximum priority
     
     %找最大的priority的patch
     %everytime in the while loop will find a highPriority
     %find the patch with maximum priority
-    max = 0;
     
     %high priority X, Y 座標
     %high priority coordinate (hpX, hpY)
+    max = 0;
     hpX = 0;
     hpY = 0;
     for i=1:indexM
         if priority(index(i,1), index(i,2) ) >= max
-            max = priority(index(i,1),index(i,2));
             hpX = index(i,1);
             hpY = index(i,2);
+            max = priority(hpX, hpY);
         end
     end
 
-    %決定highPriority 是哪塊
-    %find highPriority
-    highPriority = uint8(zeros(patch, patch, 3));
+    %填入highPriority 
+    %fill highPriority
+    highPriority = zeros(patch, patch, 3);
     for i= -patchIndex:patchIndex
        for j= -patchIndex:patchIndex
            highPriority(i+patchIndex+1,j+patchIndex+1,:) = maskedImage(hpX+i, hpY+j, :);
        end
     end
-    %figure(8);imshow(highPriority);title('highPriority');
+    %figure(6);imshow(highPriority);title('highPriority');
     
     %STEP4 find the minimum exemplar
     
@@ -175,20 +174,21 @@ while ~isequal(mask, zeros(m,n) ) %as research 等到 omega = 0為止
     
     
     %避免 碰到原圖邊界
-    min = 99999999;
+    min = 999999999;
     %to avoid "target" hit the border of originImage
     targetX = 1;
     targetY = 1;
-    target = uint8(zeros(patch, patch, 3));
+    target = zeros(patch, patch, 3);
+    
     
     %(tx,ty) is the center of patch*patch(81)
     %so (tx,ty) should in the patchIndex(4) to m-patchIndex(4) 
-    for tx=hpX-floor((patch*patch*patch)/2):hpX+floor((patch*patch*patch)/2)
+    for tx=hpX-floor((patch*patch*3)/2):hpX+floor((patch*patch*3)/2)
         %out of boundary
         if tx <= patchIndex || tx >= m-patchIndex
             continue;
         end
-        for ty=hpY-floor((patch*patch*patch)/2):hpY+floor((patch*patch*patch)/2)
+        for ty=hpY-floor((patch*patch*3)/2):hpY+floor((patch*patch*3)/2)
             %out of boundary
             if ty <= patchIndex || ty >= n-patchIndex
                 continue;
@@ -197,70 +197,76 @@ while ~isequal(mask, zeros(m,n) ) %as research 等到 omega = 0為止
             %in mask
             %inside (tx, ty) patch
             %there can't be any mask == white inside
-            
-            
             ismask = 0;
-            tmptarget = uint8(zeros(patch, patch, 3));
-            %fill the target
-            for px=-patchIndex:patchIndex
-                for py=-patchIndex:patchIndex
-                    if mask(tx+px, ty+py) == 255
+            for mx= -patchIndex:patchIndex
+                for my= -patchIndex:patchIndex
+                    if mask(tx+mx, ty+my) == 255
                         ismask = 1;
-                        break;
-                    else
+                    end
+                end
+            end
+            
+            if ismask ~= 1
+                
+                tmptarget = zeros(patch, patch, 3);
+                %fill the target
+                for px=-patchIndex:patchIndex
+                    for py=-patchIndex:patchIndex
                         tmptarget(px+patchIndex+1, py+patchIndex+1, :) = maskedImage(tx+px, ty+py,:);
                     end
                 end
-                if ismask == 1
-                    break;
-                end
-            end
-            
-            if ismask == 1
-                continue;
-            end
-            %figure(7);imshow(tmptarget);title('tmptarget');
-            %figure(8);imshow(highPriority);title('highPriority');
-            
-            
-            %compute SSD
-            SSD = 0;
-            for x=1:patch
-                for y=1:patch
-                    if mask(hpX-patchIndex-1 + x, hpY-patchIndex-1 + y) == 0
-                        L = highPriority(x,y,1) - tmptarget(x,y,1); 
-                        a = highPriority(x,y,2) - tmptarget(x,y,2);
-                        b = highPriority(x,y,3) - tmptarget(x,y,3);
-                        SSD = SSD + sqrt(double(L*L + a*a + b*b));
+
+                %figure(7);imshow(tmptarget);title('tmptarget');
+                %figure(8);imshow(highPriority);title('highPriority');
+
+
+                %compute SSD
+                SSD = 0;
+                for x=1:patch
+                    for y=1:patch
+                        if mask(hpX-patchIndex-1+x, hpY-patchIndex-1+y) == 0
+                            L = highPriority(x,y,1) - tmptarget(x,y,1); 
+                            a = highPriority(x,y,2) - tmptarget(x,y,2);
+                            b = highPriority(x,y,3) - tmptarget(x,y,3);
+                            SSD = SSD + sqrt(double(L*L + a*a + b*b));
+                        end
                     end
                 end
+                SSD = SSD / (patch*patch);
+
+                if SSD <= min
+                   min = SSD;
+                   targetX = tx;
+                   targetY = ty;
+                   target = tmptarget;
+                end
+                
             end
             
-            if SSD < min
-               min = SSD;
-               targetX = tx;
-               targetY = ty;
-               target = tmptarget;
-            end
-        end
-    end
+        end%tx
+    end%ty
+    
     %show targetX and targetY in the command line
     targetX
     targetY
     colorTransform = makecform('lab2srgb');
     targetShow = applycform(target , colorTransform);
     highPriorityShow = applycform(highPriority , colorTransform);
-    figure(7);imshow(targetShow);title('target');
-    figure(8);imshow(highPriorityShow);title('highPriority');
+    maskedImageshow = applycform(maskedImage, colorTransform);
+    figure(9);imshow(targetShow);title('target');
+    figure(10);imshow(highPriorityShow);title('highPriority');
+    figure(11);imshow(maskedImageshow);title('maskedImage before last step');
     
     %STEP5 copy image data from origin image
     
-    %ready to copy the target to the highPriority
+    %copy the target to the highPriority
     for i=-patchIndex:patchIndex
         for j=-patchIndex:patchIndex
-            maskedImage(hpX+i, hpY+j,:) = target(1+patchIndex+i, 1+patchIndex + j,:);
-            %update the mask;
-            mask(hpX+i, hpY+j) = 0;
+            %update the maskedImage and the mask
+            if(mask(hpX+i, hpY+j) == 255 )
+            	maskedImage(hpX+i, hpY+j,:) = target(1+patchIndex+i, 1+patchIndex + j,:);
+                mask(hpX+i, hpY+j) = 0;
+            end
         end 
     end
     
@@ -268,8 +274,10 @@ while ~isequal(mask, zeros(m,n) ) %as research 等到 omega = 0為止
     tmpImage = maskedImage;
     colorTransform = makecform('lab2srgb');
     tmpImage = applycform(tmpImage , colorTransform);
-    figure(9);imshow(tmpImage);title('tmpImage');
-    figure(10);imshow(mask);title('after fill in mask');
+    figure(12);imshow(tmpImage);title('tmpImage');
+    %figure(13);imshow(mask);title('after fill in mask');
+    
+    
      
     %STEP6 update the confidence
     
@@ -293,5 +301,5 @@ while ~isequal(mask, zeros(m,n) ) %as research 等到 omega = 0為止
 end
 colorTransform = makecform('lab2srgb');
 maskedImage = applycform(maskedImage , colorTransform);
-figure(3);imshow(maskedImage);
+figure(14);imshow(maskedImage);
 
